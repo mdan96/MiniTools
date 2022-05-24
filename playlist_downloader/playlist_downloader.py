86dbef22c335cd8cfd36dc1d1ebfe9fb7fb40475
mdan96 <43816474+mdan96@users.noreply.github.com>
2022-05-24 10:06:26 +0100

    Version 0.1a

---
+++ playlist_downloader/playlist_downloader.py
@@ -0,0 +1,35 @@
+from pytube import YouTube
+from pytube import Playlist
+import os 
+import re
+
+
+
+def create_playlist_dict():
+    playlist_dict = {}
+    with open('links.txt', 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            if line.startswith('['):
+                pass
+            content = line.split(',')
+            link = content[0]
+            folder_name = content[1].replace('\n','')
+            playlist_dict[link] = folder_name
+    return playlist_dict
+
+
+create_playlist_dict()
+playlist_dict = create_playlist_dict()
+for key in playlist_dict.keys():
+    link = key
+    folder_name = playlist_dict[key]
+    playlist = Playlist(key)
+    playlist.video_urls
+    parent_dir = os.getcwd()
+    path = os.path.join(parent_dir, folder_name)
+    count = 0
+    for url in playlist:
+        count += 1
+        print(count,' ',url)
+        YouTube(url).streams.filter(only_audio=True).first().download(path)
